92fd6f989e841f2e43e770507388146c9655f240
EmmanuelAyanleye <ayanleyeemmanuel5@gmail.com>
2025-04-30 22:31:53 +0100

    first commit

---
+++ student_profile/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'index.html')    
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username == 'admin' and password == 'password':  # Dummy check
+            return redirect('home')
+        else:
+            return HttpResponse("Invalid credentials")
+    return render(request, 'login.html')
